fd532936c5c1652671ae7d9582deb06d45b5d52f
iandante77 <iandan874@gmail.com>
2020-06-06 14:15:33 +0300

    porto

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const menubtn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn_burger");
+const nav = document.querySelector(".nav");
+const menunav = document.querySelector(".menu-nav");
+const navitems = document.querySelectorAll(".menu-nav_item");
+let showMenu = false;
+
+menubtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    hamburger.classList.add("open");
+    nav.classList.add("open");
+    menunav.classList.add("open");
+    navitems.forEach((item) => item.classList.add("open"));
+    showMenu = true;
+  } else {
+    hamburger.classList.remove("open");
+    nav.classList.remove("open");
+    menunav.classList.remove("open");
+    navitems.forEach((item) => item.classList.remove("open"));
+    showMenu = false;
+  }
+}
